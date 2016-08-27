5f3c698c927e2b04439a6dca0c76bde36d25516c
Nat <nescobar714@gmail.com>
2016-08-27 19:57:22 +0000

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Nesco
+
+admin.site.register(Nesco)
+# Register your models here.
---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hi(name):
+    print('Hi ' + name + '!')
+    
+girls=['Rachel','Nat','Nesco','Yazmin']
+for name in girls:
+    hi(name)
+    print('Next girl')
+    
